8a9f562017d76f278bb62e0de01e8c19df3f028d
bit-frame <linuskang09@gmail.com>
2024-12-18 13:14:17 +1000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 2700;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.redirect('/dashboard.html');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
